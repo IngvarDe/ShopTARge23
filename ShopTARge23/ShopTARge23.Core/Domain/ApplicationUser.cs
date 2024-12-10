78927000429f9c7f39391af96f0fbbc222997a30
Ingvar Derešivski <inde999@gmail.com>
2024-12-10 10:10:03 +0200

    FirstNameParam

--- ShopTARge23/ShopTARge23.Core/Domain/ApplicationUser.cs
+++ ShopTARge23/ShopTARge23.Core/Domain/ApplicationUser.cs
@@ -6,5 +6,6 @@ namespace ShopTARge23.Core.Domain
     public class ApplicationUser : IdentityUser
     {
         public string City { get; set; }
+        public string Name { get; set; }
     }
 }
